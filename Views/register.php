b309e035f958d511cdc4e206cb2858396dbd8475
JoseRodriguez98 <joj.rodriguez@duocuc.cl>
2024-10-05 09:42:30 -0300

    Update register.php

--- Views/register.php
+++ Views/register.php
@@ -15,6 +15,24 @@
   <link rel="stylesheet" href="../Util/Css/toastr.min.css">
 
 </head>
+<div class="modal fade" id="terminos" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h1 class="modal-title fs-5">Términos y condiciones</h1>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        ...
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary btn-block" data-dismiss="modal">Cerrar</button>
+      </div>
+    </div>
+  </div>
+</div>
 <body class="hold-transition login-page">
 <div class="mt-5">
   <div class="login-logo">
@@ -39,33 +57,46 @@
                 <label for="pass">Contraseña</label>
                 <input type="password" name="pass" class="form-control" id="pass" placeholder="Ingrese su contraseña">
               </div>
+              <div class="form-group">
+                <label for="nombres">Nombres</label>
+                <input type="text" name="nombres" class="form-control" id="nombres" placeholder="Ingrese su nombre">
+              </div>
+              <div class="form-group">
+                <label for="rut">RUT</label>
+                <input type="text" name="rut" class="form-control" id="rut" placeholder="Ingrese su RUT">
+              </div>
+              <div class="form-group">
+                <label for="telefono">Telefono</label>
+                <input type="text" name="telefono" class="form-control" id="telefono" placeholder="Ingrese su telefono">
+              </div>
             </div>
             <div class="col-sm-6">
               <div class="form-group">
                 <label for="pass_repeat">Repite la contraseña</label>
                 <input type="password" name="pass_repeat" class="form-control" id="pass_repeat" placeholder="Confirme su contraseña">
               </div>
+              <div class="form-group">
+                <label for="apellidos">Apellidos</label>
+                <input type="text" name="apellidos" class="form-control" id="apellidos" placeholder="Ingrese sus apellidos">
+              </div>
+              <div class="form-group">
+                <label for="email">Email</label>
+                <input type="text" name="email" class="form-control" id="email" placeholder="Ingrese su email">
+              </div>
             </div>
-          </div>
-            <div class="card-body">
-              <div class="form-group">
-                <label for="exampleInputEmail1">Email address</label>
-                <input type="email" name="email" class="form-control" id="exampleInputEmail1" placeholder="Enter email">
-              </div>
-              <div class="form-group">
-                <label for="exampleInputPassword1">Password</label>
-                <input type="password" name="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
-              </div>
+            <div class="col-sm-12">
               <div class="form-group mb-0">
                 <div class="custom-control custom-checkbox">
-                  <input type="checkbox" name="terms" class="custom-control-input" id="exampleCheck1">
-                  <label class="custom-control-label" for="exampleCheck1">I agree to the <a href="#">terms of service</a>.</label>
+                  <input type="checkbox" name="terms" class="custom-control-input" id="terms">
+                  <label class="custom-control-label" for="terms">Estoy de acuerdo con los<a href="#" data-toggle="modal" data-target="#terminos"> Términos de servicio</a>.</label>
                 </div>
               </div>
             </div>
+          </div>
+            
             <!-- /.card-body -->
-            <div class="card-footer">
-              <button type="submit" class="btn btn-primary">Submit</button>
+            <div class="card-footer text-center ellow">
+              <button type="submit" class="btn btn-lg bg-gradient-primary">Registrarme</button>
             </div>
         </form>
 
